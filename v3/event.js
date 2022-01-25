1cd92449003e1cb389d27468229bad59a6eb8700
haihan <haihanh2@163.com>
2022-01-25 14:24:43 +0800

    v3 add

---
+++ v3/event.js
@@ -0,0 +1,21 @@
+class SelfEvent{
+  constructor() {
+    this.eventList = {}
+  }
+  publish (type, ...data) {
+    this.eventList[type]?.forEach(cb => {
+      cb(...data)
+    });
+  }
+  subscribe (type, cb) {
+    if (!this.eventList[type]) {
+      this.eventList[type] = []
+    }
+    this.eventList[type].push(cb)
+  }
+  unsubscribe (type) {
+    if (this.eventList[type]) {
+      delete this.eventList[type]
+    }
+  } 
+}
